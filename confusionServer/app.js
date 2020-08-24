e4069c842153db8cba218cb9e5dd3ccb79da5105
Krishnam Rathi <krishnam.rathi@gmail.com>
2020-08-25 01:44:35 +0530

    Passport

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,25 +45,22 @@ app.use(session({
   saveUninitialized: false,
   store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+  console.log(req.user);
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 
 }
